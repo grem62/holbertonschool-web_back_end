95d1bbd9b108513a90136670ef798c06ff52e5ef
grem62 <gremontmatheo22@gmail.com>
2023-08-28 10:52:04 +0200

    add task sum float and concat string

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3
+"""
+add float with type annotation
+"""
+
+def add(a: float, b: float) -> float:
+    "return sum float"
+    return a + b
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+concatenate two string
+"""
+
+
+def concat(string1: str, string2: str) -> str:
+    "return concat string"
+    return string1 + string2
