f117a54427f3b0a72bac4f0ccbe3ea5b4677706c
grem62 <gremontmatheo22@gmail.com>
2023-08-28 14:39:15 +0200

    complexe types -mixed list

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
-Complex types - string and int/float to tuple
+Complex types - mixed list
 """
+from typing import Union, Tuple
 
-from typing import List, Union, Tuple
 
-
-def to_kv(k: str, v: List[Union[float, int]]) -> Tuple[float, int]:
-    "return tuple"
-    return k and v ** 2
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    return int or tuple(int, float)
+    """
+    return k, float(v ** 2)
