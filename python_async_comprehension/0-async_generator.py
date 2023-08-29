31d4d48df1d69ce070c0dae6a0025ec338bf0e4b
grem62 <gremontmatheo22@gmail.com>
2023-08-29 15:54:46 +0200

    correction code

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator(float):
+async def async_generator() -> Generator[float, None, None]:
     "generate async per seconde"
     for i in range(10):
         await asyncio.sleep(1)
