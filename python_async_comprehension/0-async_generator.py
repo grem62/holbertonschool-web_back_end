ac3db5c225b1bdd1310acb657a1646e8975ef8b4
grem62 <gremontmatheo22@gmail.com>
2023-08-29 15:37:00 +0200

    add readme and first task generate asyncrone

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Async Generator
+"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    "generate async per seconde"
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
