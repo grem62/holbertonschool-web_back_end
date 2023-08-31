98db875008cedfefa1ae95c39c59e915c37f9a01
grem62 <gremontmatheo22@gmail.com>
2023-08-31 11:08:40 +0200

    correction commentaire

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -11,7 +11,14 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
-    ""
+    """
+    Asynchronously collects and returns a sorted list of random delays.
+
+    :param n: The number of delays to collect (default is 0).
+    :param max_delay: The maximum value for
+    random delay generation (default is 10).
+    :return: A sorted list of randomly generated delays.
+    """
 
     delays = []
 
