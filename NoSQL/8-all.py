57d4bdbc794d28f8befea2d312e641a95d31df82
grem62 <gremontmatheo22@gmail.com>
2023-09-05 10:16:22 +0200

    return list in collection

---
+++ NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+list all file in collection with python
+"""
+import pymongo
+
+def list_all(mongo_collection):
+    "list all file in collection with python"
+    return list(mongo_collection.find())
