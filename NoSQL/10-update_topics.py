d0adffad39b07f6c74d75535d0c28169dde437bd
grem62 <gremontmatheo22@gmail.com>
2023-09-05 12:02:02 +0200

    correction code

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -3,6 +3,7 @@
 task10
 """
 
+
 def update_topics(mongo_collection, name, topics):
-    "Write a Python function that changes all topics of a school document based on the name:"
-    return mongo_collection.updatemany({name : name}, { "$set" :{topics : topics}})
+    "Write a Python function that change all topic of a school"
+    mongo_collection.updatemany({"name": name}, {"$set": {"topics": topics}})
