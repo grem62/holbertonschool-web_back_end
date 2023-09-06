4e808ea75f438d145e549e3ae50b015428fe6db9
grem62 <gremontmatheo22@gmail.com>
2023-09-06 10:49:48 +0200

    correction code

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -7,4 +7,4 @@ task9
 def insert_school(mongo_collection, **kwargs):
     "insert new objet with kwargs"
     result = mongo_collection.insert_one(kwargs)
-    return result._id
+    return result.inserted_id
