87d56c148c9a0e86f01d21edb006d521a08c4421
grem62 <gremontmatheo22@gmail.com>
2023-09-06 11:20:03 +0200

    add file index and readme

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3_summary_
+"""
+
+"""
+
+from typing import Tuple
+
+
+def index_range(page, page_size) -> Tuple[int, int]:
+    """_summary_
+
+    Args:
+        page (_type_): _description_
+        page_size (_type_): _description_
+
+    Returns:
+        tuple[int, int]: _description_
+    """
+    start_page = (page - 1) * page_size
+    end_page = (start_page + page_size)
+    return (start_page, end_page)
