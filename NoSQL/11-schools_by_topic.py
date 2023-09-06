bc5a090557a3b408eb0abed6e5787e880d197fe9
grem62 <gremontmatheo22@gmail.com>
2023-09-06 10:38:28 +0200

    task11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+task11
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    "Write a Python function that returns the list of school"
+    return mongo_collection.find({"topic": topic})
