df8c5c0d40abffc6de04ed266941cdef0eae0702
grem62 <gremontmatheo22@gmail.com>
2023-09-08 09:51:24 +0200

    log

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,40 +1,28 @@
 #!/usr/bin/env python3
-"""
-Provides some stats about Nginx logs stored in MongoDB
-"""
+"""script that provides some stats about Nginx logs stored in MongoDB """
+
 from pymongo import MongoClient
 
-
-def log_stats():
-    """
-    provides some stats about Nginx logs stored in MongoDB
-    """
-    # Connect to the MongoDB server
-    client = MongoClient('mongodb://127.0.0.1:27017')
-
-    # Access the 'logs' database and 'nginx' collection
+if __name__ == "__main__":
+    # Connexion à la base de données MongoDB
+    client = MongoClient('mongodb://localhost:27017/')
     db = client.logs
     collection = db.nginx
 
-    # Get the total number of logs
-    total_logs = collection.count_documents({})
+    # Compter le nombre total de documents dans la collection "nginx"
+    documents = collection.count_documents({})
+    # Afficher le nombre total de logs
+    print(f"{documents} logs")
 
-    # Get the counts for each HTTP method
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {method: collection.count_documents(
-        {"method": method}) for method in methods}
-
-    # Get the count of logs with method=GET and path=/status
-    status_check_count = collection.count_documents(
-        {"method": "GET", "path": "/status"})
-
-    # Print the results
-    print(f"{total_logs} logs")
     print("Methods:")
-    for method in methods:
-        print(f"\tmethod {method}:", method_counts[method])
+
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        # Compter le num de doc pour chaque méthode
+        method_count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+
+    # Compter le num de logs avec méthode GET et chemin /status
+    status_check_count = collection.count_documents({"$and": [{"path": "/status"}, {"method": "GET"}]})
     print(f"{status_check_count} status check")
 
-
-if __name__ == "__main__":
-    log_stats()
+    client.close()
