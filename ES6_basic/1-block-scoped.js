7ae8cf88ce16ecc6b09b7d5e45771946cb59e849
grem62 <gremontmatheo22@gmail.com>
2023-09-13 13:04:44 +0200

    Block Scope

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true; // Utilisation de 'let' pour éviter la réaffectation
+    const task2 = false; // Utilisation de 'let' pour éviter la réaffectation
+  }
+
+  return [task, task2];
+}
