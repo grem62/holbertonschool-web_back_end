faa177d966469587a523c6046893cbb0e713b05a
grem62 <gremontmatheo22@gmail.com>
2023-09-15 10:20:15 +0200

    correction

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-unused-vars */
+
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    //Block Scope
-    const task = true; // Utilisation de 'let' pour éviter la réaffectation
-    const task2 = false; // Utilisation de 'let' pour éviter la réaffectation
+    // estlint-disable-next-line no-unused-vars
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
